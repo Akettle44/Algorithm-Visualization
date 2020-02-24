e293a6f6a7d1847ee4a5323a40fe15c29254aeaf
Akettle44 <andrewjkettle@gmail.com>
2020-02-23 20:09:54 -0800

    Calculating framerate through clock and total frames

--- algos.cpp
+++ algos.cpp
@@ -7,38 +7,47 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
 
 using namespace cv;
 
 Mat insertion_sort(Mat image, int height, int width)
 {
+	std::clock_t start;
+	double duration;
+	int count = 0;
 	Mat cop;
 	int p;
 	Vec3b key;
 	Vec3b val;
-	VideoWriter video("akoutput.avi",cv::CAP_FFMPEG,cv::VideoWriter::fourcc('H', '2', '6', '4'),50,Size(200, 200),true);
+	//VideoWriter video("akoutput.avi",cv::CAP_FFMPEG,cv::VideoWriter::fourcc('H', '2', '6', '4'),600,Size(width, height),true);
 
+	start = std::clock();
 	for(int i = 0; i < height; i++)
 	{
-		for(int j = 1; j < width; j++) /* needs to start at 1 because arrays begin at 0 */
+		for(int j = 1; j < width; j++) // needs to start at 1 because arrays begin at 0 
 		{
 			key = image.at<Vec3b>(Point(j,i));
 
 			p = j - 1;
 			val = image.at<Vec3b>(Point(p,i));
 
-			while(p > -1  && (val[0] > key[0])) /* checks greater than -1 because arrays have 0 index */
+			while(p > -1  && (val[0] > key[0])) // checks greater than -1 because arrays have 0 index 
 			{
 				image.at<Vec3b>(Point(p+1, i)) = image.at<Vec3b>(Point(p,i)); 
 				p--;
 				val = image.at<Vec3b>(Point(p,i));
 			}
 			image.at<Vec3b>(Point(p+1,i)) = key;
-			cop = image.clone();
-			cvtColor(cop, cop, cv::COLOR_HSV2BGR);
-			video << cop;
+			//cop = image.clone();
+			cvtColor(image, cop, cv::COLOR_HSV2BGR);
+//			video << cop;
+			count++;
 		}
 	}
+	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    std::cout << "Amoount of time: " << duration <<'\n';
+	std::cout << "Output frames: " << count;
 	return image;
 }
 
@@ -53,7 +62,9 @@ int main(int, char**)
 	Mat hsv(height, width, CV_8UC3);
 	//cvtColor(image1, hsv, cv::COLOR_BGR2HSV);
 
-	/* initialzation */
+	srand(time(0)); //seeds random number generator
+
+	// initialzation 
 	for(int i = 0; i < width; i++)
 	{
 		for(int j = 0; j < height; j++)
@@ -66,8 +77,8 @@ int main(int, char**)
 		}
 	}
 
+	// insertion sort testing 
 	hsv = insertion_sort(hsv, height, width);
-
 	namedWindow("Display Window", WINDOW_AUTOSIZE);
 	cvtColor(hsv, hsv, cv::COLOR_HSV2BGR);
 	imshow("Display Window", hsv);
